bd37bdbc9facdf214afd725c9f433392e4ed3f9e
Jeremy Lothian <jeremy.lothian@accuweather.com>
2017-03-23 16:02:41 -0400

    Add project files.

---
+++ SharpROM.MUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharpROM.MUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
